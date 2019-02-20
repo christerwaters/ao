541e284db63c12bb987e073b1f8cdde02d7f35a6
christerwaters <christer@christerwaters.com>
2019-02-20 22:08:58 +0100

    Initial Upload

---
+++ js/rndm.js
@@ -0,0 +1,6 @@
+
+for(var i = 0; i < randomize.length; i++){
+    var target = Math.floor(Math.random() * randomize.length -1) + 1;
+    var target2 = Math.floor(Math.random() * randomize.length -1) +1;
+    randomize.eq(target).before(randomize.eq(target2));
+}
---
+++ js/theme.js
@@ -0,0 +1,18 @@
+// Script
+lastScroll = 0;
+$(window).on('scroll',function() {    
+    var scroll = $(window).scrollTop();
+    if(lastScroll - scroll > 0) {
+        $("header").addClass("s--up");
+    } else {
+        $("header").removeClass("s--up");
+    }
+    lastScroll = scroll;
+
+    $(window).scroll(function () {
+        var top_offset = $(window).scrollTop();
+        if (top_offset == 0) {
+            $('header').removeClass('s--up');
+        }
+    })	
+});
