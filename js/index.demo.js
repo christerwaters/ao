996d95a07f6b10d50e5ebffb3e52de799f908e60
christerwaters <christer@christerwaters.com>
2019-03-03 21:53:19 +0100

    Completed the demo layout.

--- js/index.demo.js
+++ js/index.demo.js
@@ -2,41 +2,38 @@ var song;
 var button;
 
 function setup() {
-	var canvas = createCanvas(200, 200);
+	var canvas = createCanvas(windowWidth, windowHeight);
 	canvas.parent('sketch-holder');
 	song = loadSound("audio/demo.mp3", loaded);
 	amp = new p5.Amplitude();
 }
 
-function changeBG() {
-	background(song.currentTime() * random(),song.currentTime() * vol,song.currentTime() * random());
-}
-
 function loaded() {
 	console.log("loaded");
 	song.setVolume(0.5);
-	button = createButton("play");
+	button = createButton('<i class="fas fa-play-circle class="txt-hover-white"" style="font-size:34px !important;"></i><span style="margin-left:10px;font-size:34px !important;font-family: Averta;" class="txt-hover-white">PLAY DEMO</span>');
+	button.parent("button-holder");
 	button.mousePressed(togglePlaying);
 }
 
 function togglePlaying() {
 	if (!song.isPlaying()){
 		song.play();
-		button.html('<i class="fas fa-pause-circle"></i>');
+		button.html('<i class="fas fa-pause-circle class="txt-hover-white"" style="font-size:34px !important;"></i><span style="margin-left:10px;font-size:34px !important;font-family: Averta;" class="txt-hover-white">PAUSE DEMO</span>');
 	} else {
 		song.pause();
-		button.html('<i class="fas fa-play-circle"></i>');
+		button.html('<i class="fas fa-play-circle class="txt-hover-white"" style="font-size:34px !important;"></i><span style="margin-left:10px;font-size:34px !important;font-family: Averta;" class="txt-hover-white">PLAY DEMO</span>');
 	}
 }
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 function draw() {
-	background(song.currentTime() * 10,0,233);
+	background(0,0,0,0);
 	vol = amp.getLevel();
-	diam = map(vol,0,0.5,0,1)
-	fill(255,0,0);
-	ellipse(width/2, height/2,width*diam,height*diam);
-	fill(0,255,0);
-	ellipse(width/2, height/2,width*diam*0.8,height*diam*0.8);
-	fill(0,0,255);
-	ellipse(width/2, height/2,width*diam*0.5,height*diam*0.5);
+	diam = map(vol,0,0.5,0,1);
+	diamx = map(vol,0,1,100,255);
+	fill(diamx,255,diamx);
+	ellipse(width/2, height/2,height*diam,height*diam);
+	
 }
