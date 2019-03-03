6af636649400579550cb4ef530079d664de7a32b
christerwaters <christer@christerwaters.com>
2019-03-03 19:03:48 +0100

    Update index.demo.js

--- js/index.demo.js
+++ js/index.demo.js
@@ -4,21 +4,39 @@ var button;
 function setup() {
 	createCanvas(200, 200);
 	song = loadSound("audio/demo.mp3", loaded);
-	song.setVolume(0.5);
-	slider = createSlider(0, 1, 0.5, 0.01);
-	button = createButton("play");
-	button.mousePressed(togglePlaying);
+	amp = new p5.Amplitude();
+}
+
+function changeBG() {
+	background(song.currentTime() * random(),song.currentTime() * vol,song.currentTime() * random());
 }
 
 function loaded() {
 	console.log("loaded");
+	song.setVolume(0.5);
+	button = createButton("play");
+	button.mousePressed(togglePlaying);
 }
 
 function togglePlaying() {
-	song.play();
+	if (!song.isPlaying()){
+		song.play();
+		button.html('<i class="fas fa-pause-circle"></i>');
+	} else {
+		song.pause();
+		button.html('<i class="fas fa-play-circle"></i>');
+	}
 }
 
 function draw() {
-	background(0);
-	song.setVolume(slider.value());
+	background(song.currentTime() * 10,0,233);
+	
+	vol = amp.getLevel();
+	diam = map(vol,0,0.5,0,1)
+	fill(255,0,0);
+	ellipse(width/2, height/2,width*diam,height*diam);
+	fill(0,255,0);
+	ellipse(width/2, height/2,width*diam*0.8,height*diam*0.8);
+	fill(0,0,255);
+	ellipse(width/2, height/2,width*diam*0.5,height*diam*0.5);
 }
