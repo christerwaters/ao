bce17f81e434b1c579aa632a27932b677dfeb75a
christerwaters <christer@christerwaters.com>
2019-03-28 15:50:55 +0100

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,10 +1,10 @@
 $(document).bind('mousemove', function(e){
     $('.tail').css({
-       left:  e.pageX + Math.floor((Math.random() * 20) + 10),
-       top:   e.pageY + Math.floor((Math.random() * 10) + 1)
+       left:  e.pageX + Math.floor((Math.random() * 5) + 1),
+       top:   e.pageY + Math.floor((Math.random() * 5) + 1)
     });
-    $('.tail').css({
-       left:  e.pageX + Math.floor((Math.random() * 20) + 10),
-       top:   e.pageY + Math.floor((Math.random() * 10) + 1)
-    });
+    //$('.tail').css({
+    //   left:  e.pageX + Math.floor((Math.random() * 5) + 1),
+    //   top:   e.pageY + Math.floor((Math.random() * 5) + 1)
+    //});
 });
