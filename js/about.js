eaf2359e791ad4de1adf4c5aabcbfa9d9893ba78
christerwaters <christer@christerwaters.com>
2019-03-29 21:29:18 +0100

    Layout changes to /about

--- js/about.js
+++ js/about.js
@@ -1,7 +1,6 @@
 $(document).bind('mousemove', function(e){
     $('.tail').css({
-       left:  e.pageX + Math.floor((Math.random() * 5) + 1),
-       top:   e.pageY + Math.floor((Math.random() * 5) + 1)
+       top:   e.pageY + Math.floor((Math.random() * 5) + 1)-200
     });
     //$('.tail').css({
     //   left:  e.pageX + Math.floor((Math.random() * 5) + 1),
