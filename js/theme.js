bed4acdf55331bf04bfea4d149c8b9d706ef47c6
christerwaters <christer@christerwaters.com>
2019-05-15 12:59:46 +0200

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -52,20 +52,20 @@ $(window).on('scroll',function() {
 
 });
 
-
+function GetURLParameter(sParam){
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++)
+  {
+      var sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] == sParam)
+      {
+          return sParameterName[1];
+      }
+  }
+}​
 $(document).ready(function() {
-  function GetURLParameter(sParam){
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam)
-        {
-            return sParameterName[1];
-        }
-    }
-  }​
+
   var page = GetURLParameter('page');
   $(document.body).addClass(page);
 
